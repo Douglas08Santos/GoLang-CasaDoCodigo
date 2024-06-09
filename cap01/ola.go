d8c6105f235f0a1b9e3bd561d2bd8d5549a7108c
Douglas08Santos <santosdouglas0808@gmail.com>
2024-06-09 12:11:37 -0300

    Cap01 - adicionando comentários

--- cap01/ola.go
+++ cap01/ola.go
@@ -1,7 +1,25 @@
+/*
+# Arquivos-fonte devem seguir sempre a mesma estrutura, dividida em três seções:
+*/
+
+/*
+# Declaração do pacote
+
+# Todo código Go deve obrigatoriamente existir dentro de um pacote
+
+# Todo programa em Go deve ter um pacote 'main' contendo um função main()
+*/
 package main
 
+/*
+# Declaraçao dos pacotes externos dos quais o arquivo-fonte depende
+*/
 import "fmt"
 
+/*
+# Código referente ao programa ou pacote
+# A função main() não recebe nenhum argumento e não retorna nenhum valor
+*/
 func main() {
 	fmt.Println("Olá, Go!")
 }
